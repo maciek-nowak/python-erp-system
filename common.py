245464ec4f5a235a5fdd72457dbfc0dd71bdad71
Maciej Nowak <mcnowak@wp.pl>
2017-06-06 18:51:21 +0200

    Merged with common branch

--- common.py
+++ common.py
@@ -8,6 +8,7 @@ import random
 # @table: list of lists
 # @generated: string - randomly generated string (unique in the @table)
 
+
 def generate_random(table):
     """
     Generates random and unique string. Used for id/key generation.
@@ -18,21 +19,16 @@ def generate_random(table):
     """
     generated = ''
 
-    #letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o,','p','q','r','s','t','u','v','w','x','y','z']
-    #losujemy i dodajemy
-    #wielkie od 65 do 90 włacznie
-    #male od 97 do 122 włacznie
-    #znaki 33 do 47 włacznie
     checkout = True
     while checkout:
-        generated += chr(random.randrange(97,123))
-        generated += chr(random.randrange(65,91))
-        generated += str(random.randrange(0,10))
-        generated += str(random.randrange(0,10))
-        generated += chr(random.randrange(65,91))
-        generated += chr(random.randrange(97,123))
+        generated += chr(random.randrange(97, 123))
+        generated += chr(random.randrange(65, 91))
+        generated += str(random.randrange(0, 10))
+        generated += str(random.randrange(0, 10))
+        generated += chr(random.randrange(65, 91))
+        generated += chr(random.randrange(97, 123))
         for i in range(2):
-            generated += chr(random.randrange(33,48))
+            generated += chr(random.randrange(33, 48))
         for index in range(len(table)):
             if generated == table[index][0]:
                 checkout = True
